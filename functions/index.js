d0c0aeec659b10ae71830e41cc4da6a1d1fd501e
nminchow <noelminchow@gmail.com>
2018-12-02 22:02:23 -0600

    webhook on game created

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const request = require('request');
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -52,7 +53,7 @@ exports.redirectAfterHeaders = functions.https.onRequest((req, res) => {
     <head>
       <link id="oembed" type="application/json+oembed" href="https://${process.env.GCP_PROJECT}.firebaseapp.com/${gameId}/oembed.json">
       <meta name="theme-color" content="#56077a">
-      <meta content='/artifinder.png' property='og:image'>
+      <meta name="twitter:image" content="https://artifinder.com/artifinder.png" />
     </head>
     <body>
       <script>window.location="https://artifinder.com/?gameId=${gameId}";</script>
@@ -60,3 +61,56 @@ exports.redirectAfterHeaders = functions.https.onRequest((req, res) => {
   </html>`;
   res.status(200).send(result);
 });
+
+exports.createGame = functions.firestore
+  .document('games/{gameId}')
+  .onCreate((snap, context) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const game = snap.data();
+    const payload = {
+      "username": "Artifinder",
+      "avatar_url": "https://artifinder.com/artifinder.png",
+      "content": "New Tournament Posted on [Artifinder](https://artifinder.com)",
+      "embeds": [
+        {
+          "thumbnail": {
+            "url": "https://artifinder.com/artifinder.png"
+          },
+          "color": 5638010,
+          "author": {
+            "name": game.description,
+            "url": `https://artifinder.com/${snap.id}`,
+          },
+          "fields": [
+            {
+              "name": game.type,
+              "value": `${game.playerLimit} Player`
+            },
+          ],
+          "footer": {
+            "text": "Artifinder - Find and Host Artifact Tournaments"
+          },
+        }
+      ],
+    }
+    admin.firestore().collection('/listeners').get().then((listeners) => {
+      listeners.forEach((listener) => {
+        const data = listener.data();
+        console.log(`called listener: ${JSON.stringify(payload)}`);
+        var options = {
+          method: 'post',
+          body: payload,
+          json: true,
+          url: data.url,
+        }
+        request(options, (err, res, body) => {
+          if (err) {
+            console.log(`error posting: ${err}`);
+          }
+        });
+      })
+      return;
+    }).catch(() => {});
+    return;
+  });
